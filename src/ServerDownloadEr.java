8f835a30baee5e4d132ed4bfe1868ea6ee5af567
7777a2333 <109708109+7777a2333@users.noreply.github.com>
2023-01-19 22:10:41 +0800

    Add files via upload

--- src/ServerDownloadEr.java
+++ src/ServerDownloadEr.java
@@ -5,34 +5,36 @@ import java.util.Scanner;
 public class ServerDownloadEr {
     //Main主方法,程序的入口
     public static void main(String[] args) {
-        System.out.println("服务端下载器  作者:哔哩哔哩@7777a2333");
-        System.out.println("开源地址:https://github.com/7777a2333/MinecraftServerDownloader");
-        System.out.println("采用GNU GPLv3协议分发,并含有附加条款");
-        System.out.println("原版服务端Mojang源使用了mcjars.com跳转至Mojang服务器");
-        System.out.println("原版服务端国内源由BMCLAPI和MCBBS提供高速下载服务.BMCLAPI为公益服务.赞助BMCLAPI可以帮助作者提供更好的高速下载服务");
-        System.out.println("赞助BMCLAPI:https://afdian.net/a/bangbang93");
-        System.out.println("CraftBukkit和Spigot使用了Getbukkit服务器下载");
-        //显示关于
-        System.out.println("-----------------服务端支持版本-----------------");
-        System.out.println("原版服务端:         :全版本 本程序仅支持下载1.2.5及以上(包括快照)");
-        System.out.println("Purpur服务端       :1.14.1及以上");
-        System.out.println("Spigot服务端       :1.0.0及以上");
-        System.out.println("CraftBukkit服务端  :1.0.0及以上");
+        版本();
+        //显示版本
+
         System.out.println("---------------------------------------------");
-        //显示各个服务端支持版本
+
+        说明();
+        //调用"说明"方法 显示说明
+
+        显示支持版本();
+        //调用"显示支持版本"方法 显示支持版本
+
         Scanner 要求输入 = new Scanner(System.in);
+        //新建Scanner命名为"要求输入"
+
         System.out.println("输入你要下载的服务端版本号(例如1.19.3 | 22w13a(仅快照原版服务端))");
         String 服务端版本 = 要求输入.next();
         //要求输入服务端版本,并保存到"服务端版本"String变量内
+
         System.out.println("输入你要下载的服务端:(填序号)");
-        System.out.println("1   原版服务端   2 Purpur服务端(Parper分支)  3   Spigot服务端   4 CarftBukkit服务端");
+        System.out.println("1   原版服务端   2 Purpur服务端(Parper分支)  3   Spigot服务端   4 CraftBukkit服务端");
         int 服务端类型 = 要求输入.nextInt();
         //要求输入服务端序号,并且放进"服务端类型"int变量内
+
         String 服务端下载链接 = null;
         String 服务端名称 = null;
-        //声明变量
+        //初始化变量为null
+
         if (服务端类型 == 1) {
             //如果服务端类型=1运行此处的代码(开始)
+            System.out.println("选择你要使用的镜像源:(填序号)");
             System.out.println("1 Mojang源 2 BMCLAPI源 3 MCBBS源");
             int 下载源 = 要求输入.nextInt();
             //要求输入下载源编号
@@ -83,22 +85,53 @@ public class ServerDownloadEr {
 
         }
         //判断服务端类型并将下载链接放到"服务端下载链接"变量(方法有点蠢别在意)(后期维护爆炸)
+
         try {
             Runtime.getRuntime().exec("cmd /k start ./wget.exe " + 服务端下载链接);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         //使用Wget下载服务端
+
         System.out.println("下载完成后,输入true启动服务端,也可在服务端所在目录运行命令java -jar " + 服务端名称 + "运行服务端.");
+        //显示输入true启动的说明
+
         boolean Start = 要求输入.nextBoolean();
         if (Start){
             try {
                 Runtime.getRuntime().exec("cmd /k start java -jar " + 服务端名称 );
             } catch (IOException e) {
                 throw new RuntimeException(e);
+                //启动服务端的代码
             }
         }else{
             System.exit(233);
+            //退出的代码
         }
+    }
+
+    public static void 显示支持版本() {
+        System.out.println("-----------------服务端支持版本-----------------");
+        System.out.println("原版服务端:         :全版本 本程序仅支持下载1.2.5及以上(包括快照)");
+        System.out.println("Purpur服务端       :1.14.1及以上");
+        System.out.println("Spigot服务端       :1.0.0及以上");
+        System.out.println("CraftBukkit服务端  :1.0.0及以上");
+        System.out.println("---------------------------------------------");
+        //显示各个服务端支持版本
+    }
+
+    public static void 说明() {
+        System.out.println("服务端下载器  作者:哔哩哔哩@7777a2333");
+        System.out.println("开源地址:https://github.com/7777a2333/MinecraftServerDownloader");
+        System.out.println("采用GNU GPLv3协议分发,并含有附加条款");
+        System.out.println("原版服务端Mojang源使用了mcjars.com跳转至Mojang服务器");
+        System.out.println("原版服务端国内源由BMCLAPI和MCBBS提供高速下载服务.BMCLAPI为公益服务.赞助BMCLAPI可以帮助作者提供更好的高速下载服务");
+        System.out.println("赞助BMCLAPI:https://afdian.net/a/bangbang93");
+        System.out.println("CraftBukkit和Spigot使用了Getbukkit服务器下载");
+        //显示关于
+    }
+    public static void 版本(){
+        System.out.println("版本:1.4.0\n" +
+                "编译时间: 2022/1/19 22:04");
     }
 }
